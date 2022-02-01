0c940b8cfd060354e68201ba8abd11fe0d9b166e
tbenedek92 <t.benedek92@gmail.com>
2022-02-01 11:49:09 +0100

    producer

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # python_beanstalk
 Interaction with beanstalk queue
+
+Docker container for beanstalkd:
+
+```docker run -d -p 11300:11300 schickling/beanstalkd```
+
+[beanstalkc](https://github.com/earl/beanstalkc) library works with python 2.7
---
+++ producer.py
@@ -0,0 +1,14 @@
+import beanstalkc
+import time
+
+queue_name = "default"
+num_of_messages_to_produce = 100
+
+beanstalk = beanstalkc.Connection(host='localhost', port=11300)
+beanstalk.use(queue_name)
+
+count = 1
+while count <= num_of_messages_to_produce:
+    beanstalk.put('{"name":"job' + str(count) + '","value":"test_value' + str(count) + '"}')
+    print('Job ' + str(count) + ' sent!')
+    count+=1
